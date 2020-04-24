64efa4e2abff04d25222afff488ca5b1888c047c
Joseph Romero <joeromero47@gmail.com>
2020-04-24 18:58:41 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,12 +3,17 @@ def third_challenge
    :montague => {
       :patriarch => {
         #your key/value pairs here
+        :name => "Lord Montague"
+        :age => "53"
         },
       :matriarch => {
         #your key/value pairs here
+        :name => "Lady Montague"
+        :age => "54"
         },
       :hero => {
         #your key/value pairs here
+        :name => 
         },
       :hero_friends => []
    }, 
