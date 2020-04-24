a77c3d5c3e90721a7009c0943d676b1b5ec259d9
Joseph Romero <joeromero47@gmail.com>
2020-04-24 19:00:03 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -13,13 +13,17 @@ def third_challenge
         },
       :hero => {
         #your key/value pairs here
-        :name => 
+        :name => "Romeo"
+        :age => "15"
+        :status => "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
         #your key/value pairs here
+        :name => "Lord Capulet"
+        :age => "50"
         },
       :matriarch => {
         #your key/value pairs here
