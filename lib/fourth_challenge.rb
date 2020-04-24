655014499526a45ed151cf6e25d57540ed160a6d
Joseph Romero <joeromero47@gmail.com>
2020-04-24 19:14:15 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -17,11 +17,8 @@ def fourth_challenge
       :heroine_friends => [
         #your hashes here!
         {name: "Steven", age: "30", attitude: "confused"},
-        {name: "Nurse", age: "44", attitude: "worried}
+        {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
 }
-
-  
-
 end
